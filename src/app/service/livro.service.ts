89072124880df5f5523bce6a5f3101ba62adc6e1
S4NDMAN11 <joaoluizlopespimenta@gmail.com>
2023-02-05 17:45:46 -0300

    ✨ Implementando mecanismos de busca e otimização de recursos do servidor.

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {LivrosResultado} from "../models/interfaces";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +13,9 @@ export class LivroService {
   constructor(private http: HttpClient) {
   }
 
-  buscar(valorDigitado: string) {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado)
 
-    return this.http.get(this.API, {params})
+    return this.http.get<LivrosResultado>(this.API, {params})
   }
 }
--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {LivroService} from "../../service/livro.service";
+import {catchError, debounceTime, filter, map, switchMap, throwError} from "rxjs";
+import {Item, LivrosResultado} from "../../models/interfaces";
+import {LivroVolumeInfo} from "../../models/livroVolumeInfo";
+import {FormControl} from "@angular/forms";
+
+const PAUSA = 3000
 
 @Component({
   selector: 'app-lista-livros',
@@ -7,16 +13,30 @@ import {LivroService} from "../../service/livro.service";
   styleUrls: ['./lista-livros.component.css']
 })
 export class ListaLivrosComponent {
+  campoBusca = new FormControl
+  mensagemErro = ''
+  livrosResultado: LivrosResultado
 
-  listaLivros: [];
-  campoBusca: string = ''
+  constructor(private service: LivroService) {
+  }
 
-  constructor(private service: LivroService) { }
+  livrosEncontrados$ = this.campoBusca.valueChanges.pipe(
+    debounceTime(PAUSA),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    switchMap((valorDigitado) => this.service.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    map(resultado => resultado.items ?? []),
+    map(items => this.livrosResultadoParaLivros(items)),
+    catchError(erro => {
+      console.log(erro);
+      return throwError(() =>
+        new Error(this.mensagemErro = `Ops, ocorreu um erro! Recarregue a aplicação!`));
+    })
+  );
 
-  buscarLivros() {
-    this.service.buscar(this.campoBusca)
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item)
+    })
   }
 }
-
-
-
